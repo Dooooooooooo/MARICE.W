b1d34247255aaed51f1d8e2006c8b02f0b37dd4b
3c1u <kelotti.erio@gmail.com>
2018-08-23 23:23:13 +0900

    Revert "Merge branch '3c1u-branch' into master"
    This reverts commit 26cb383cbc0f4bb105f01a3e08acd68fa17f8b43, reversing
    changes made to 48d04bf8ea8718914bd3d777e4401dae078b6863.

--- Unity Project/MALICE.W/Assets/Prefabs/Database/Jsonfiles/datagen.py
+++ Unity Project/MALICE.W/Assets/Prefabs/Database/Jsonfiles/datagen.py
@@ -1,13 +1,11 @@
 #generate empty chara_data_*.json in the specified range
 
 import sys
-import os
 
-PATH_TO_JSONFILES = os.path.dirname(os.path.abspath(__file__)) + "/../Unity Project/MALICE.W/Assets/Prefabs/Database/Jsonfiles"
-placeholder       = '{"m_ID":%s,"m_NAME":"","m_SEX":"","m_HIGHSCORE":0,"m_PLAYTIME":0}'
+placeholder = '{"m_ID":%s,"m_NAME":"","m_SEX":"","m_HIGHSCORE":0,"m_PLAYTIME":0}'
 
 def make_file(id):
-    fp = open('%s/chara_data_%s.json' % (PATH_TO_JSONFILES, str(id)), 'w')
+    fp = open('chara_data_%s.json' % str(id), 'w')
     fp.write(placeholder % str(id))
     fp.close()
 
@@ -24,7 +22,7 @@ def main(args):
             end   = int(param[1])
             print("Create file No. %d - %d..." % (start, end), end=' ')
             
-            for i in range(start, end + 1):
+            for i in range(start, end):
                 make_file(i)
             
             print("Done.")
