a9876ca1963b75992b0fe4d3a37467871b3803f0
御法川凌太 <>
2018-09-02 22:35:20 +0900

    戦闘シーンの土台?

---
+++ Unity Project/MALICE.W/Assets/Scripts/Battle/Elixir.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Elixir : MonoBehaviour
+{
+
+    private int starttime = 0;
+    private int now = 0;
+    private int num = 1;
+
+    void Update()
+    {
+        if (num != 1)
+        {
+            this.GetComponent<Text>().text = "0";
+            now = DateTime.Now.Hour * 60 * 60 + DateTime.Now.Minute * 60 + DateTime.Now.Second;
+            if (now - starttime >= 100)
+            {
+                num = 1;
+                starttime = 0;
+            }
+        }
+
+        else
+        {
+            this.GetComponent<Text>().text = num.ToString();
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                starttime = DateTime.Now.Hour * 60 * 60 + DateTime.Now.Minute * 60 + DateTime.Now.Second;
+                num = 0;
+            }
+        }
+    }
+}
---
+++ Unity Project/MALICE.W/Assets/Scripts/UI/Slider/HPText.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HPText : MonoBehaviour
+{
+    public int hp = 100;
+
+    void Update()
+    {
+        this.GetComponent<Text>().text = hp.ToString();
+    }
+}
